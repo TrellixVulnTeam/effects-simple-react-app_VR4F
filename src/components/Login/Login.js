57da955e75258b55c2c83dcd8675d03a7c5e4531
Anthony Limani <alimani9193@gmail.com>
2022-06-14 16:37:21 -0400

    Refactored login form so that the inputs are in their own reusable component Input.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import AuthContext from "../../context/store/auth-context";
 
 
 import Card from '../UI/Card/Card';
+import Input from "../UI/Input/Input";
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
@@ -79,34 +80,22 @@ const Login = (props) => {
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                <div
-                    className={`${classes.control} ${
-                        emailState.isValid === false ? classes.invalid : ''
-                    }`}
-                >
-                    <label htmlFor="email">E-Mail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={emailState.value}
-                        onChange={emailChangeHandler}
-                        onBlur={validateEmailHandler}
-                    />
-                </div>
-                <div
-                    className={`${classes.control} ${
-                        passwordState.isValid === false ? classes.invalid : ''
-                    }`}
-                >
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={passwordState.value}
-                        onChange={passwordChangeHandler}
-                        onBlur={validatePasswordHandler}
-                    />
-                </div>
+                <Input
+                    id="email"
+                    label="E-Mail"
+                    type="email"
+                    isValid={emailIsValid}
+                    onChange={emailChangeHandler}
+                    onBlur={validateEmailHandler}
+                />
+                <Input
+                    id="password"
+                    label="Password"
+                    type="password"
+                    isValid={passwordIsValid}
+                    onChange={passwordChangeHandler}
+                    onBlur={validatePasswordHandler}
+                />
                 <div className={classes.actions}>
                     <Button type="submit" className={classes.btn} disabled={!formIsValid}>
                         Login
