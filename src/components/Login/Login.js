2b464991641add170e0c5515e85c1b0133ed9447
Anthony Limani <alimani9193@gmail.com>
2022-06-15 10:06:15 -0400

    Using refs to control the focus on the Login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useReducer, useContext} from 'react';
+import React, {useState, useEffect, useReducer, useContext, useRef} from 'react';
 import AuthContext from "../../context/store/auth-context";
 
 
@@ -72,15 +72,25 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        authCtx.onLogin(emailState.value, passwordState.value);
+        if (formIsValid) {
+            authCtx.onLogin(emailState.value, passwordState.value);
+        } else if (!emailIsValid) {
+            emailInputRef.current.focus();
+        } else {
+            passwordInputRef.current.focus();
+        }
     };
 
     const authCtx = useContext(AuthContext);
+
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
                 <Input
+                    ref={emailInputRef}
                     id="email"
                     label="E-Mail"
                     type="email"
@@ -89,6 +99,7 @@ const Login = (props) => {
                     onBlur={validateEmailHandler}
                 />
                 <Input
+                    ref={passwordInputRef}
                     id="password"
                     label="Password"
                     type="password"
@@ -97,7 +108,7 @@ const Login = (props) => {
                     onBlur={validatePasswordHandler}
                 />
                 <div className={classes.actions}>
-                    <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+                    <Button type="submit" className={classes.btn}>
                         Login
                     </Button>
                 </div>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useImperativeHandle, useRef} from "react";
 import classes from "./Input.module.css";
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
+
+    const inputRef = useRef();
+
+    const activate = () => {
+        inputRef.current.focus();
+    };
+    useImperativeHandle(ref, () => {
+        return {
+            focus: activate
+        };
+    });
 
     return (
         <React.Fragment>
@@ -12,6 +23,7 @@ const Input = props => {
             >
                 <label htmlFor={props.id}>{props.label}</label>
                 <input
+                    ref={inputRef}
                     type={props.type}
                     id={props.id}
                     value={props.value}
@@ -21,6 +33,6 @@ const Input = props => {
             </div>
         </React.Fragment>
     )
-};
+});
 
 export default Input;
